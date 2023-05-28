3e5eb055746fcf58053e6975aecfde12cd5a8552
skyhereYet <mr.mvpopov@yandex.ru>
2023-05-28 11:09:10 +0300

    feat: create user and film controllers

--- src/main/java/Spring/Web/controller/ValidationException.java
+++
@@ -1,8 +0,0 @@
-package spring.web.controller;
-
-public class ValidationException extends IllegalArgumentException {
-
-    public ValidationException(String message) {
-        super(message);
-    }
-}
